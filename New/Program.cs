6d9c316270a8a314bb296b39be3071a6d0e08e65
Fiyaz Bin Hasan <fiyazhasan@live.com>
2021-12-31 13:18:30 +0600

    merge new project

---
+++ New/Program.cs
@@ -0,0 +1,23 @@
+﻿var factory = new Factory<Bar>();
+Console.WriteLine(factory);
+
+public class Foo
+{
+    public Foo(int a)
+    {
+
+    }
+}
+
+public class Bar
+{
+
+}
+
+class Factory<T> where T : new()
+{
+    public T Create()
+    {
+        return new T();
+    }
+}
